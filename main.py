6f86760c6f86b53558e279dcc6d70385cd5a36dd
Kuralay-02 <72wsdydm8d@privaterelay.appleid.com>
2026-01-22 17:56:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -134,14 +134,14 @@ async def handle_username(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(text)
 
 # ================== MAIN ==================
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(on_button))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_username))
 
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
